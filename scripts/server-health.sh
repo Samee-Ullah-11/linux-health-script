f3c58245d59a9bc55385885a1977d37d12111924
Samee Ullah <sameeullah634@gmail.com>
2026-09-08 00:27:12 +0500

    add linux health check script

---
+++ scripts/server-health.sh
@@ -0,0 +1,27 @@
+echo  "======================================"
+echo  "         server check"
+echo  "======================================="
+
+echo  ""
+echo "Hostname:"
+hostname
+
+echo ""
+echo "current user:"
+whoami
+
+echo ""
+echo "data and time:"
+date
+
+echo ""
+echo "system uptime:"
+uptime
+
+echo ""
+echo "CPU information:"
+nproc
+
+echo ""
+echo "CPU uptime:"
+uptime
