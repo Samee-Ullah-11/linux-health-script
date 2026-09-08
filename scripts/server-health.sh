b8024696dfea1119bf04956c9342e6c733ef1030
Samee Ullah <sameeullah634@gmail.com>
2026-09-09 00:16:59 +0500

    adding more scripts

--- scripts/server-health.sh
+++ scripts/server-health.sh
@@ -25,3 +25,30 @@ nproc
 echo ""
 echo "CPU uptime:"
 uptime
+
+echo ""
+echo "Memory usage:"
+free -h
+
+echo ""
+echo "disk usage:"
+df -h
+
+
+echo ""
+echo "Network connection:"
+if ping -c 2 google.com > /dev/null 2>&1; then
+   echo  "internet connection : OK "
+else
+   echo  "Internet connection : Failed"
+fi
+
+echo ""
+echo "service status:"
+if systemctl is-active --quiet cron; then
+   echo "Cron service: runnig"
+else
+   echo "Cron service: stopped"
+fi
+
+
